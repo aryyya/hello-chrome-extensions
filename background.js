6353b2bb6040d571cb3447d94c59822d701103d0
aryyya <aryyya.xyz@gmail.com>
2019-04-19 15:03:30 -0700

    Show icon on certain developer domain.

--- background.js
+++ background.js
@@ -4,4 +4,12 @@ chrome.runtime.onInstalled.addListener(() => {
   }, () => {
     console.log('The color is green.')
   })
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [ new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: { hostEquals: 'developer.chrome.com' }
+      })],
+      actions: [ new chrome.declarativeContent.ShowPageAction() ]
+    }])
+  })
 })
