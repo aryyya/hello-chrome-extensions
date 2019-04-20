083fce84da6d45229289f13c5797af3fec1fb63a
aryyya <aryyya.xyz@gmail.com>
2019-04-20 11:55:25 -0700

    Put functionality in extension popup.

--- popup.js
+++ popup.js
@@ -1,21 +1,40 @@
-const changeColor = document.getElementById('change-color')
+// const changeColor = document.getElementById('change-color')
 
-chrome.storage.sync.get('color', data => {
-  changeColor.style.backgroundColor = data.color
-  changeColor.setAttribute('value', data.color)
-})
+// chrome.storage.sync.get('color', data => {
+//   changeColor.style.backgroundColor = data.color
+//   changeColor.setAttribute('value', data.color)
+// })
 
-changeColor.onclick = element => {
-  const color = element.target.value
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    chrome.tabs.executeScript(
-      tabs[0].id,
-      {
-        code: `
-          document.body.style.transition = '1s ease all';
-          document.body.style.backgroundColor = '${color}';
-        `
-      }
-    )
+// changeColor.onclick = element => {
+//   const color = element.target.value
+//   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+//     chrome.tabs.executeScript(
+//       tabs[0].id,
+//       {
+//         code: `
+//           document.body.style.transition = '1s ease all';
+//           document.body.style.backgroundColor = '${color}';
+//         `
+//       }
+//     )
+//   })
+// }
+
+const buttons = Array.from(document.getElementsByClassName('button'))
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const color = button.dataset.color
+    
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      chrome.tabs.executeScript(
+        tabs[0].id,
+        {
+          code: `
+            document.body.style.transition = '1s ease all';
+            document.body.style.backgroundColor = '${color}';
+          `
+        }
+      )
+    })
   })
-}
+})
